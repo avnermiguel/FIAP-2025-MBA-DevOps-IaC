ddfa82aa90f086c96d068f2e25caaa5d213f3a3e
Avner Santos <avner_miguel@hotmail.com>
2025-09-03 22:59:46 -0300

    Teste

--- IaC/backend.tf
+++ IaC/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-avner-santos"  # Usar o nome do bucket criado pelo Terraform
     key            = "terraform.tfstate" # Nome do arquivo de estado
     region         = "us-east-1"      # Substitua pela sua região
-    dynamodb_table = "terraform-locks" # Usar o nome da tabela DynamoDB criada pelo Terraform
+    # dynamodb_table = "terraform-locks" # Usar o nome da tabela DynamoDB criada pelo Terraform
     encrypt        = true             # Habilitar a criptografia do State no S3
   }
 }
